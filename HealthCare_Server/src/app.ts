9126104ce76b7b5d413c79e5dbf3eddf53db3b76
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-11-07 21:22:26 +0600

    Routes handled

--- HealthCare_Server/src/app.ts
+++ HealthCare_Server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoutes } from "./app/modules/User/user.routes";
 import { adminRoutes } from "./app/modules/admin/admin.routes";
+import router from "./app/routes";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoutes);
+// app.use("/api/v1/user", userRoutes);
+// app.use("/api/v1/admin", adminRoutes);
+
+app.use("/api/v1", router);
 export default app;
