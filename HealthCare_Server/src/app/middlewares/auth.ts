112741c5b717c90f200cd6acc89db6f98ac1b053
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-11-14 19:32:25 +0600

    AppError Initiated

---
+++ HealthCare_Server/src/app/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtHelper } from "../modules/helper/generateToken";
+import config from "../../config";
+import ApiError from "../errors/ApiError";
+
+const auth = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      throw new ApiError(
+        httpStatus.INTERNAL_SERVER_ERROR,
+        "You are not authorized!"
+      );
+    }
+    const verifiedUser = jwtHelper.verifyToken(
+      token,
+      config.jwt.jwt_secret as string
+    );
+
+    if (verifiedUser && !roles.includes(verifiedUser.role)) {
+      throw new ApiError(
+        httpStatus.INTERNAL_SERVER_ERROR,
+        "You are not authorized!"
+      );
+    }
+    next();
+  };
+};
+
+export default auth;
