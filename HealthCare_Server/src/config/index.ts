02f84cb7ccfb03d40fcda61cbc4314148f8e27e5
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-11-14 18:58:31 +0600

    env setop

---
+++ HealthCare_Server/src/config/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+  post: process.env.PORT,
+  environment: process.env.NODE_ENV,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    jwt_expires_in: process.env.JWT_EXPIRES_IN,
+    refreshToken: process.env.REFRESH_TOKEN,
+    refreshToken_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+  },
+};
