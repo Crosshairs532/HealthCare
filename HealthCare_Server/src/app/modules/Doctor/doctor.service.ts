eb15bc323625c20d8ac8b813beda03d102b37d2a
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-11-30 06:19:27 +0600

    Doctor Update

--- HealthCare_Server/src/app/modules/Doctor/doctor.service.ts
+++ HealthCare_Server/src/app/modules/Doctor/doctor.service.ts
@@ -1,19 +1,33 @@
 import prisma from "../../utils/prisma";
 
 const updateIntoDB = async (id: string, payload: any) => {
+  const { specialties, ...DoctorData } = payload;
   const doctorData = await prisma.doctor.findUniqueOrThrow({
     where: {
       id,
     },
   });
 
-  const updateData = await prisma.doctor.update({
-    where: {
-      id,
-    },
-    data: payload,
+  const result = await prisma.$transaction(async (tx) => {
+    const updateData = await tx.doctor.update({
+      where: {
+        id,
+      },
+      data: DoctorData,
+    });
+
+    for (const sId of specialties) {
+      const createDoctorSpecialties = await tx.doctorSpecialties.create({
+        data: {
+          doctorId: updateData?.id,
+          specialitiesid: sId,
+        },
+      });
+    }
+
+    return updateData;
   });
 
-  return updateData;
+  return result;
 };
 export const doctorService = { updateIntoDB };
